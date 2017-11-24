beae150269497d29d7113f2b7de32c870f96216f
anirudh314 <anirudh3.14a@gmail.com>
2017-11-24 11:12:32 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        JFrame frame = new JFrame("simple application");
+        frame.setSize(new Dimension(800, 600));
+        frame.setVisible(true);
+
+		
+
+    }
+
+
+
+
+
+
+
+}
